0ca2934128f2fe8aa6f69dd506e3245cf4e86d6c
Solarna Finance <solarna.io@protonmail.com>
2021-11-22 17:00:13 +0700

    first commit

---
+++ precache-manifest.05fe54f73746ffcf9a5f2a4a6e2d7c00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6fc056b720ba490a3937960618d5c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a86125194a9a4abfc8b8",
+    "url": "/static/css/2.1434370d.chunk.css"
+  },
+  {
+    "revision": "b2211886b1bc0113470c",
+    "url": "/static/css/main.b6544cb5.chunk.css"
+  },
+  {
+    "revision": "a86125194a9a4abfc8b8",
+    "url": "/static/js/2.62a4a624.chunk.js"
+  },
+  {
+    "revision": "4a4bcbcfcc085c06cd762672e644f1f2",
+    "url": "/static/js/2.62a4a624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2211886b1bc0113470c",
+    "url": "/static/js/main.cfc93ea2.chunk.js"
+  },
+  {
+    "revision": "ff8347c2a84f5138b487",
+    "url": "/static/js/runtime-main.766003da.js"
+  },
+  {
+    "revision": "e58deb6d85380c82738de3789d20947e",
+    "url": "/static/media/logo.e58deb6d.png"
+  }
+]);
